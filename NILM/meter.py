432ecf8ec2ed07c24bc83e5ea63ff890a8035a45
thibautlavril <thibaut_lavril@hotmail.fr>
2014-12-18 17:07:20 -0200

    load metadata meter and user

--- NILM/meter.py
+++ NILM/meter.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
+from collections import OrderedDict
+from store import Store
+
+
 class Meter(object):
 
     def __init__(self):
-        meter.metadata = dict()
-        meter.address = dict()
+        self.metadata = dict()
+        self.store = str()
 
-    def load(self, user):
-            self.address = {'file': user.file, 'key': key}
-            meter_data = {'phases': 2, 'sampling':1}
-            location_data = {}  
+    def load(self, user, meter_name):
+            self.store = user.store
+            assert meter_name in user.metadata['meters']
+            self.metadata = user.metadata['meters'][meter_name]
+
+if __name__ == '__main__':
+    from user import User
+    hdf_filename = '/Volumes/Stockage/DATA/DATA_BLUED/CONVERTED/user1.h5'
+    user1 = User()
+    user1.load(hdf_filename)
+    meter1_name = user1.metadata['meters'].keys()[0]
+    meter1 = Meter()
+    meter1.load(user1, meter1_name)
--- NILM/user.py
+++ NILM/user.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import pandas as pd
+from meter import Meter
 
 
 class User(object):
@@ -8,26 +9,30 @@ class User(object):
     Represent a user of nilm. He is defined by an ID, a location and a file
     on the database where its data is stored. A user can have several meters.
 
-    attributes: file, meters, metadata.
+    attributes: store, meters, metadata.
     """
 
-    def __init__(self, hdf_filename):
-        assert os.path.isfile(hdf_filename)
-        self.file = hdf_filename
+    def __init__(self):
+        self.store = str()
         self.meters = dict()
         self.metadata = dict()
 
-    def load(self):
+    def load(self, hdf_filename):
         """"
         Load the metadata of the file to create the meters and the metadata of
         our user
         """
-        with pd.get_store(self.file) as store:
+        assert os.path.isfile(hdf_filename)
+        self.store = hdf_filename
+        with pd.get_store(self.store) as store:
             self.metadata = store.root._v_attrs.metadata
-
+        for meter_name in self.metadata['meters'].keys():
+            meter = Meter()
+            meter.load(self, meter_name)
+            self.meters[meter_name] = meter
 
 if __name__ == "__main__":
     hdf_filename = '/Volumes/Stockage/DATA/DATA_BLUED/CONVERTED/user1.h5'
-    user1 = User(hdf_filename)
-    user1.load()
-    print user1.metadata
+    user1 = User()
+    user1.load(hdf_filename)
+    print user1.metadata['meters'].keys()
