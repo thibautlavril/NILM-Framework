fc2bc11d1de06b2ef9db5f82f1ce155ec271a069
thibautlavril <thibaut_lavril@hotmail.fr>
2015-01-12 12:25:15 -0200

    update events, detection on 2 phases simultaneously

--- NILM/events.py
+++ NILM/events.py
@@ -3,8 +3,12 @@ import detection
 
 
 class Events(pd.DataFrame):
+    
+    detection_types = {
+            "simple_edge": detection.simple_edge,
+            "steady_states": detection.steady_states
+        }
 
-    detection_types = ['simple_edge', 'steady_states']
 
     def __init__(self, meter):
         super(Events, self).__init__()
@@ -14,19 +18,20 @@ class Events(pd.DataFrame):
     def meter(self):
         return self._meter
 
-    def detection(self, detection_type, **kwargs):
+    def detection(self, detection_type, phases_separation=False, **kwargs):
         assert (detection_type in Events.detection_types)
         phases = self.meter.measurements.columns.levels[0]
+        detection_func = Events.detection_types[detection_type]
         df = pd.DataFrame()
-        detectionDict = {
-            "simple_edge": detection.simple_edge,
-            "steady_states": detection.steady_states
-        }
-        for phase in phases:
-            dff = detectionDict[detection_type](self.meter.measurements[phase],
-                                                **kwargs)
-            dff['phase'] = phase
-            df = df.append(dff)
+        
+        if phases_separation:
+            for phase in phases:
+                dff = detection_func(self.meter.measurements[phase], **kwargs)
+                dff['phase'] = phase
+                df = df.append(dff)
+        else:
+            
+            df = detection_func(self.meter.measurements, **kwargs)
 
         super(Events, self).__init__(df)
 
