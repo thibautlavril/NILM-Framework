652b3fe7821108ccc019b931b2b2904b2f8ef882
thibautlavril <thibaut_lavril@hotmail.fr>
2015-01-15 19:36:05 -0200

    update appliance_models: Test and implementation of association by dbscan

--- NILM/appliance_models.py
+++ NILM/appliance_models.py
@@ -10,49 +10,21 @@ import numpy as np
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.cluster import AffinityPropagation, DBSCAN
 
-  
-def metric_cluster(x1, x2):
+def euclidian_cluster(x1, x2):
     assert x1.shape == x2.shape
     return  np.linalg.norm(x1 + x2)
-        
-    
-def construct_distance_matrix(clusters, threshold_distance):
-    """
-    Construct a distance matrix between each clusters
-
-    Parameters
-    ----------
-    clusters: NILM.clusters.Cluster object
-
-    Returns
-    -------
-    distance_matrix_list = dict
-        key: phase
-        value: distance matrix between clusters for this phase
-    """
-    assert clusters.phase_by_phase
-    phases = clusters.index.levels[0]
-    power_types = clusters._features
-    d_dict = {}
-    for phase in phases:
-        df = clusters.ix[phase]
-        X = df[power_types].values
-        d = pairwise_distances(X, metric=metric_cluster)
-        d_dict[phase] = d
-    return d_dict
 
 
-def simple_association(X, threshold_distance):
-
+def simple_association(X, distance_threshold, metric):
     #  Construct the distance matrix D
-    D = pairwise_distances(X, metric=metric_cluster)
+    D = pairwise_distances(X, metric=metric)
 
     # Initialization
     d = 0  # Distance between two clusters
     n_appli = 0
     appliances = -1*np.ones_like(D[0])  # appliances set at -1
 
-    while d < threshold_distance:
+    while d < distance_threshold:
 
         # Find the new min > d
         D_masked = D[D > d]
@@ -66,26 +38,69 @@ def simple_association(X, threshold_distance):
         for (x, y) in zip(xxx, yyy):
             if (appliances[x] == -1) & (appliances[y] == -1):
                 # if x and y doesn't have appliance, set them at n_appli
-                n_appli += 1
                 appliances[x] = n_appli
                 appliances[y] = n_appli
+                n_appli += 1
 
     return appliances
 
 
+def dbscan_association(X, metric=euclidian_cluster, **dbscan_parameters):
+    #  Construct the distance matrix D
+    D = pairwise_distances(X, metric=euclidian_cluster)
+    #  Compute a DBSCAN clustering for a distance matrix
+    model = DBSCAN(metric='precomputed', **dbscan_parameters)
+    appliances = model.fit_predict(D)
+    return appliances
+
+
+def affinity_propagation_association(X, **dbscan_parameters):
+    pass
+    
+
+
+associationsDict = {
+    "simple": {
+        "model": simple_association,
+        "parameters": {
+            'distance_threshold': 35,
+            'metric': euclidian_cluster
+            }
+        },
+    "dbscan": {
+        "model": dbscan_association,
+        "parameters": {
+            'eps': 35,
+            'min_samples': 1,
+            'metric': euclidian_cluster
+            }
+        }   
+    }
+
+
 class BuildApplianceModels(object):
 
-    def __init__(self, kind, **parameters):
-        self.kind = kind
-        self.parameters = parameters
-        self.model = None
+    def __init__(self, name, **parameters):
+        # Check name of method for association is valid
+        assert name in associationsDict
+        self.name = name
+
+        # define model and default parameters from the dict
+        Dict = associationsDict[name]
+
+        self.model = Dict['model']
+
+        self.parameters = Dict['parameters']
+        # Add the parameters from **parameters in the dict
+        for k, v in parameters.iteritems():
+            self.parameters[k] = v
 
 
 class ApplianceModels(pd.DataFrame):
 
-    def __init__(self, kind, parameters):
+    def __init__(self, name, **parameters):
         super(ApplianceModels, self).__init__()
-        self.building_model = BuildApplianceModels(kind, parameters)
+        self.building_model = BuildApplianceModels(name, **parameters)
 
     def build_appliance_models(self, meter):
         #  Check that the clustering on meter was done
@@ -104,7 +119,7 @@ class ApplianceModels(pd.DataFrame):
         assert(meter.phase_by_phase)
 
         #  Initialization
-        appliances = []
+        appliances = np.array([])
 
         for phase in phases:
             #  Select the clusters of this phase
@@ -112,37 +127,26 @@ class ApplianceModels(pd.DataFrame):
             # Select the powers
             X = df[powers].values
 
-            # Associate the clusters
-            # TO DO replace by building_model.model
-            appl = simple_association(X, threshold_distance)
-            appliances.append(appl)
+            # Associate the clusters, the model use and the parameters
+            # are stored in the building_model object
+            model = self.building_model.model
+            parameters = self.building_model.parameters
+            appl = model(X, **parameters)
+            appliances = np.append(appliances, appl)
 
         # Add appliances label to meter.clusters
         appliances = np.array(appliances)
-        self.clusters_appliances_ = appliances
         meter.clusters['appliances'] = appliances
-        
+
         # Construct the pd.DataFrame Appliances Model
         df = meter.clusters.reset_index()
-        df = df.set_index(['phase', 'appliances'])
+        df = df.set_index(['phase', 'appliances', 'cluster'])
+        df = df.sortlevel([0, 1])
         super(ApplianceModels, self).__init__(df)
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        
+
 
 if __name__ == '__main__':
-    clusters = meter1.clusters
-    dd = construct_distance_matrix(clusters, distance)
-    D = dd['B']
-    model = DBSCAN(eps = 50, min_samples=1, metric='precomputed')
-    print model.fit_predict(D)
+    appliance_models = ApplianceModels('dbscan')
+    appliance_models.build_appliance_models(meter1)
+
         
