e137ee997967badb994acce8c8c048a42b3c649c
thibautlavril <thibaut_lavril@hotmail.fr>
2015-01-15 15:21:33 -0200

    update applianceModel: simple association of clusters

--- NILM/appliance_models.py
+++ NILM/appliance_models.py
@@ -10,12 +10,7 @@ import numpy as np
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.cluster import AffinityPropagation, DBSCAN
 
-class BuildApplianceModels(object):
-    
-    def __init__(self, kind, parameters):
-        self.kind = kind
-        self.parameters = parameters
-    
+  
 def distance(x1, x2):
     assert x1.shape == x2.shape
     return  np.linalg.norm(x1 + x2)
@@ -46,6 +41,32 @@ def construct_distance_matrix(clusters, distance):
         d_dict[phase] = d
     return d_dict
 
+def simple_association(D, threshold_similarity):
+    d = 0
+    distance = D.argmin(0)
+    n_appli = 0
+    associations = -1*np.ones_like(D[0])
+    while d < threshold_similarity:
+        xx, yy = np.indices(D.shape)
+        D_masked = np.ma.array(D, mask = (D<=d))
+        d = D_masked.min()
+        mask_position = (D == d)
+        xxx = xx[mask_position]
+        yyy = yy[mask_position]
+
+        for (x, y) in zip(xxx, yyy):
+            if (associations[x] == -1) & (associations[y] == -1):
+                n_appli += 1
+                associations[x] = n_appli
+                associations[y] = n_appli
+
+
+class BuildApplianceModels(object):
+
+    def __init__(self, kind, **parameters):
+        self.kind = kind
+        self.parameters = parameters
+
 
 class ApplianceModels(pd.DataFrame):
 
@@ -61,7 +82,7 @@ class ApplianceModels(pd.DataFrame):
 if __name__ == '__main__':
     clusters = meter1.clusters
     dd = construct_distance_matrix(clusters, distance)
-    d = dd['B']
+    D = dd['B']
     model = DBSCAN(eps = 50, min_samples=1, metric='precomputed')
-    print model.fit_predict(d)
+    print model.fit_predict(D)
         
