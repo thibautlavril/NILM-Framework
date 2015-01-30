90e39e04df2f1d4d75e4c652a522f42b88cbc0db
thibautlavril <thibaut_lavril@hotmail.fr>
2015-01-30 11:09:46 -0200

    update user: add docstring

--- NILM/user.py
+++ NILM/user.py
@@ -5,19 +5,50 @@ from meter import Meter
 
 
 class User(object):
-    """
-    Represent a user of nilm. He is defined by an ID, a location and a file
-    on the database where its data is stored. A user can have several meters.
+    """ Represent a user.
 
-    attributes: store, meters, metadata.
+    Each user is composed by a user metadata and a list of NILM.Meter.
+    NILM.User are useful to store a dataset with severals meters.
+
+    Parameters
+    ----------
+    hdf_filename: str
+        Name (with relative or absolute path) of the HDFS file "*.h5" where
+        the user is store. To create such files see NILM.converter module.
+
+    ID: str, optional
+        Name of the user. It is the name of the hdf_filename by default.
+
+
+    Attributes
+    ----------
+    ID: str
+        Name of the user. It is the name of the hdf_filename by default.
+
+    metadata: dict of str
+        Metadata stored in the HDFS file. It has informations on the user,
+        the meters belonging to the user: meters ID, measurements of the meters
+        etc ...
+
+    filename: str
+        Name of the file (with path) where the user is stored.
+
+    meters_ID: list of str
+        List with the meters_ID of the NILM.Meter belonging to the user.
+
+    meters: list of NILM.Meter
+        List containing the NILM.Meter objects.
     """
 
-    def __init__(self, hdf_filename):
+    def __init__(self, hdf_filename, ID=None):
         assert os.path.isfile(hdf_filename)
         with pd.get_store(hdf_filename) as store:
             metadata = store.root._v_attrs.metadata
 
-        self.ID = hdf_filename.split('/')[-1]
+        if ID is None:
+            self.ID = hdf_filename.split('/')[-1]
+        else:
+            self.ID = ID
         self.metadata = metadata
         self.filename = hdf_filename
 
